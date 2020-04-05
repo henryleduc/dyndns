9c294ae22718dfa38819477ace56c8126e59a1e0
henryleduc <unset>
2020-04-05 01:04:35 +0100

    Update main.go - Need to finish off to bring the rest of the functionality together and also add some helper functionality to update get public ip and set dns records etc.

--- cmd/main.go
+++ cmd/main.go
@@ -2,26 +2,37 @@ package main
 
 import (
 	"fmt"
+	"github.com/henryleduc/dyndns/pkg/cloudflare"
+	"github.com/henryleduc/dyndns/pkg/ip"
 	"log"
 	"os"
 
 	"github.com/google/uuid"
-
-	"github.com/henryleduc/dyndns"
 )
 
 func main() {
 	cloudflareEmail := os.Getenv("CLOUDFLARE_API_EMAIL")
 	cloudflareKey := os.Getenv("CLOUDFLARE_API_KEY")
 	cloudflareZoneID := os.Getenv("CLOUDFLARE_API_ZONEID")
-	if cloudflareEmail == "" || cloudflareKey == "" || cloudflareZoneID == "" {
-		log.Fatalf("environment variables CLOUDFLARE_API_EMAIL && CLOUDFLARE_API_KEY were not defined")
+	domain := os.Getenv("ROOT_DOMAIN")
+	subdomains := os.Getenv("SUBDOMAINS")
+
+	if cloudflareEmail == "" || cloudflareKey == "" || cloudflareZoneID == "" || domain == "" || subdomains == "" {
+		log.Fatalf("environment variables not defined the following environments variables are required: " +
+			"CLOUDFLARE_API_EMAIL and CLOUDFLARE_API_KEY and ROOT_DOMAIN and SUBDOMAINS")
 	}
 
 	zoneID := uuid.MustParse(cloudflareZoneID)
 	client := cloudflare.NewClient(cloudflareEmail, cloudflareKey, zoneID)
 
-	_, err := client.GetAllZones()
-	fmt.Println(err)
+	_, _ = client.GetAllDNSRecords()
+	//fmt.Println(err)
+	//body, err := ioutil.ReadAll(resp.Body)
+	//fmt.Println(resp.Request.URL.RequestURI())
+	//fmt.Println(resp.Status)
+	//fmt.Println(string(body))
 
+	ipAdr, err := ip.GetIPv4()
+	fmt.Println(ipAdr)
+	fmt.Println(err)
 }
